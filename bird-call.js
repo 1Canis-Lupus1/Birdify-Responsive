f9ec9c5cbc9eea24717a277737cf2eea482799c8
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2021-01-21 09:46:35 +0530

    Minor Modifications made

--- bird-call.js
+++ bird-call.js
@@ -1,1 +1,70 @@
 console.log("Bird-Calls.js connected");
+
+document.getElementById("sparrow").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("mockingbird").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("creeper").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("nightjar").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("owl").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("bushtit").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("longspur").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("nighthawk").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("raven").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("elf-owl").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("starling").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("kinglet").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("gackle").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("kingfisher").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("towhee").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("lark").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
+document.getElementById("goldfinch").addEventListener("click",function(){
+    console.log("Clicked")
+})
+
